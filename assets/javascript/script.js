4e0fd6d1231cb9d1aecc94df26ce24460e4dd63d
Matthew Standish <mwstandish@aol.com>
2023-03-06 15:42:13 -0500

    added checks for country with city

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,9 +6,9 @@ var forecastEl = document.querySelector("#forecast");
 var searchHistoryEl = document.querySelector("#search-history");
 var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
 
-function getWeather(city) {
+function getWeather(city, countryCode) {
   fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+    `https://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&units=metric&appid=${apiKey}`
   )
     .then(function (response) {
       return response.json();
@@ -30,39 +30,47 @@ function getWeather(city) {
 }
 
 function displayCurrentWeather(data) {
-    var fahrenheitTemp = (data.main.temp * 9) / 5 + 32;
-    currentWeatherEl.innerHTML = `
+  var fahrenheitTemp = (data.main.temp * 9) / 5 + 32;
+  currentWeatherEl.innerHTML = `
       <h2>${data.name} (${new Date().toLocaleDateString()})</h2>
       <div>Temperature: ${fahrenheitTemp.toFixed(1)} &deg;F</div>
       <div>Humidity: ${data.main.humidity}%</div>
       <div>Wind Speed: ${data.wind.speed.toFixed(1)} MPH</div>
     `;
-  }
+}
 
-
-  function displayError(message) {
-    var errorEl = document.querySelector(".error");
-    if (errorEl) {
-      errorEl.remove();
-    }
-    errorEl = document.createElement("div");
-    errorEl.classList.add("error");
-    errorEl.textContent = message;
-    currentWeatherEl.appendChild(errorEl);
+function displayError(message) {
+  var errorEl = document.querySelector(".error");
+  if (errorEl) {
+    errorEl.remove();
   }
-  
-  searchBtn.addEventListener("click", function (event) {
+  errorEl = document.createElement("div");
+  errorEl.classList.add("error");
+  errorEl.textContent = message;
+  currentWeatherEl.appendChild(errorEl);
+}
+
+searchBtn.addEventListener("click", function (event) {
     event.preventDefault();
-    var city = searchInput.value.trim();
-    if (city) {
-      currentWeatherEl.innerHTML = "";
-      getWeather(city);
-      searchHistory.push(city);
-      localStorage.setItem("search", JSON.stringify(searchHistory));
-      displaySearchHistory();
-      searchInput.value = "";
-    } else {
+    var cityInput = searchInput.value.trim();
+    if (!cityInput) {
       displayError("Please enter a city name");
+      return;
     }
+    var city = "";
+    var countryCode = "";
+    if (cityInput.includes(",")) {
+      var parts = cityInput.split(",");
+      city = parts[0].trim();
+      countryCode = parts[1].trim().toUpperCase();
+    } else {
+      city = cityInput;
+    }
+    getWeather(city, countryCode);
+    searchHistory.push(cityInput);
+    localStorage.setItem("search", JSON.stringify(searchHistory));
+    displaySearchHistory();
+    searchInput.value = "";
+    currentWeatherEl.removeChild(currentWeatherEl.lastChild);
   });
   
