2dfeaf2f33b912d5aea8b863393fb6b04d0c4e6a
Matthew Standish <mwstandish@aol.com>
2023-03-06 13:43:32 -0500

    added API fetch

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,3 +5,27 @@ var currentWeatherEl = document.querySelector("#current-weather");
 var forecastEl = document.querySelector("#forecast");
 var searchHistoryEl = document.querySelector("#search-history");
 var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+
+function getWeather(city) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      displayCurrentWeather(data);
+      var lat = data.coord.lat;
+      var lon = data.coord.lon;
+      return fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`
+      );
+    })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      displayForecast(data);
+    });
+}
+
